7212e054da4a12fd6ee1fb34b641ff1e9fa1d0aa
Cheveux <gaspard.szuszkin@gmail.com>
2026-03-17 12:38:15 +0100

    syling quizz component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,12 @@ import {Routes, Route } from 'react-router-dom'
 import './App.css'
 import Article from './components/articles'
 import ArticlePage from './components/articlePage'
-import Background from './components/img_url_generator/background'
 import ConversationPage from './components/conversationPage/conversation'
 import DelusionalQuiz from './quiz/DelusionalQuiz'
 
 function App() {
   return (
     <>
-      <Background />
       <Routes>
         <Route path="/" element={<Article />} />
         <Route path="/article/:id" element={<ArticlePage />} />
