c50b6b4ac0b9e2455e546429a4decfe6a82dcf9f
Cheveux <gaspard.szuszkin@gmail.com>
2026-04-17 16:09:22 +0200

    fix: update vite base path for production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/nowhere_and_4ever/' : '/',
+  base: '/',
   plugins: [react()],
   server: {
     port: 3000,
