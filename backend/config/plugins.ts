1b704ac3b4c8891028421bfba586b1c5c4edcc0a
Cheveux <gaspard.szuszkin@gmail.com>
2026-06-01 18:45:10 +0200

    new buckets on supabase for media content in strapi

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -2,4 +2,23 @@ import type { Core } from '@strapi/strapi';
 
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
 
-export default config;
+// backend/config/plugins.ts
+export default {
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        credentials: {
+          accessKeyId: process.env.SUPABASE_S3_ACCESS_KEY,
+          secretAccessKey: process.env.SUPABASE_S3_SECRET_KEY,
+        },
+        region: process.env.SUPABASE_S3_REGION,
+        endpoint: process.env.SUPABASE_S3_ENDPOINT,
+        forcePathStyle: true, // obligatoire pour Supabase
+        params: {
+          Bucket: process.env.SUPABASE_S3_BUCKET,
+        },
+      },
+    },
+  },
+};
